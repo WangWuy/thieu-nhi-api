300d22ea68fe595576f7c7912bf5019881c5a39f
Huynh Quang Huy <huyhq@overate-vntech.com>
2025-07-27 10:58:19 +0700

    Add domain cors

--- server.js
+++ server.js
@@ -29,7 +29,7 @@ app.use(generalLimiter);
 const corsOptions = {
     origin: process.env.NODE_ENV === 'production' 
         ? process.env.FRONTEND_URL?.split(',') || ['https://your-frontend-domain.com']
-        : process.env.FRONTEND_URL?.split(',') || ['http://localhost:3000', 'http://localhost:5173', 'http://127.0.0.1:3000', 'https://297fc1833232.ngrok-free.app'],
+        : process.env.FRONTEND_URL?.split(',') || ['http://localhost:3000', 'http://localhost:5173', 'http://127.0.0.1:3000', 'http://localhost:4173'],
     credentials: true,
     optionsSuccessStatus: 200,
     methods: ['GET', 'POST', 'PUT', 'DELETE', 'PATCH', 'OPTIONS'],
